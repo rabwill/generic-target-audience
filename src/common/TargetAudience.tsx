7edf94ad911b37739c82c1f7a57571a9acc02b1b
Rabia Williams <rabiawilliams@gmail.com>
2020-01-29 20:10:29 +1000

    cleaning up

--- src/common/TargetAudience.tsx
+++ src/common/TargetAudience.tsx
@@ -5,41 +5,37 @@ import { PageContext } from "@microsoft/sp-page-context";
 export interface ITargetAudienceProps {
     pageContext:PageContext;
     groupIds: IPropertyFieldGroupOrPerson[];
-    children?:any;
+    children?:JSX.Element;
 }
 export interface ITargetAudienceState {
-    children:any;
+    children:JSX.Element;
     canView?: boolean;
 }
 export default class TargetAudience extends React.Component<ITargetAudienceProps, ITargetAudienceState>{
+    constructor(props: ITargetAudienceProps) {
+        super(props);
+        this.state = {
+            canView: false,
+            children: this.props.children
+        } as ITargetAudienceState;
 
-        constructor(props: ITargetAudienceProps) {
-            super(props);
-            this.state = {
-                canView: false,
-                children:this.props.children
-            } as ITargetAudienceState;
-    
-        }
-        public componentDidMount():void{
-            //needed to make the target audience work
-            this.checkUserCanViewWebpart();   
-          }
-       
-        public checkUserCanViewWebpart():void{
-            const self = this;
-            let proms = [];
-            const _sv = new spservices();
-            self.props.groupIds.map((item) => {
-                proms.push(_sv.isMember(item.fullName, self.props.pageContext.legacyPageContext[`userId`], self.props.pageContext.site.absoluteUrl));
-            });
-            Promise.race(proms).then(val => {
-                this.setState({ canView: true }); //atleast one promise resolved
-            });
-        }
-      
-    
+    }
+    public componentDidMount(): void {
+        //setting the state whether user has permission to view webpart
+        this.checkUserCanViewWebpart();
+    }
     public render(): JSX.Element {
-        return (<div>{this.state.canView?this.state.children:`Can't touch this!`}</div>);
+        return (<div>{this.state.canView ? this.state.children : `Can't touch this!`}</div>);
+    }
+    public checkUserCanViewWebpart(): void {
+        const self = this;
+        let proms = [];
+        const _sv = new spservices();
+        self.props.groupIds.map((item) => {
+            proms.push(_sv.isMember(item.fullName, self.props.pageContext.legacyPageContext[`userId`], self.props.pageContext.site.absoluteUrl));
+        });
+        Promise.race(proms).then(val => {
+            this.setState({ canView: true }); //atleast one promise resolved
+        });
     }
 }
